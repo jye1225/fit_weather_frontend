30259b71a3d6b17d5b396ba5410bddd245901e0f
yeeun <yeeun7117@sookmyung.ac.kr>
2024-06-26 09:48:10 +0900

    0625ver2

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import KakaoLogin from "./KakaoLogin"; // 카카오 로그인 컴포넌트 import
-import NaverLogin from "./NaverLogin"; // 네이버 로그인 컴포넌트 import
 import axios from "axios";
+import "../../css/login.css";
 
 export default function Login() {
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
   const [autoLogin, setAutoLogin] = useState(false);
+  const [idError, setIdError] = useState("");
+  const [pwError, setPwError] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,8 +19,32 @@ export default function Login() {
     }
   }, []);
 
+  const validateId = (value) => {
+    const idPattern = /^[a-zA-Z0-9]+$/; // 영문과 숫자만 허용
+    if (!idPattern.test(value)) {
+      setIdError("올바른 아이디를 입력해주세요.");
+    } else {
+      setIdError("");
+    }
+  };
+
+  const validatePw = (value) => {
+    const pwPattern =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+    if (!pwPattern.test(value)) {
+      setPwError("영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.");
+    } else {
+      setPwError("");
+    }
+  };
+
   const handleLogin = async (e) => {
     e.preventDefault();
+    validateId(id);
+    validatePw(pw);
+    if (idError || pwError) {
+      return;
+    }
     try {
       const response = await axios.post("/signin", { id, password: pw });
       if (autoLogin) {
@@ -43,35 +69,39 @@ export default function Login() {
       <div className="titleWrap fontHead2">로그인</div>
 
       <div className="contentWrap">
-        <div className="inputTitle">아이디</div>
+        <div className="inputTitle fontTitleXL">아이디</div>
         <div className="inputWrap">
           <input
             className="input"
-            placeholder="사용할 아이디를 입력하세요."
+            placeholder="아이디를 입력하세요."
             value={id}
-            onChange={(e) => setId(e.target.value)}
+            onChange={(e) => {
+              setId(e.target.value);
+              validateId(e.target.value);
+            }}
           />
         </div>
-        <div className="errorMessageWrap">올바른 아이디를 입력해주세요.</div>
+        {idError && <div className="errorMessageWrap">{idError}</div>}
 
-        <div style={{ marginTop: "26px" }} className="inputTitle">
+        <div style={{ marginTop: "26px" }} className="inputTitle fontTitleXL">
           비밀번호
         </div>
         <div className="inputWrap">
           <input
             type="password"
             className="input"
-            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            placeholder="비밀번호를 입력하세요."
             value={pw}
-            onChange={(e) => setPw(e.target.value)}
+            onChange={(e) => {
+              setPw(e.target.value);
+              validatePw(e.target.value);
+            }}
           />
         </div>
-        <div className="errorMessageWrap">
-          영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.
-        </div>
+        {pwError && <div className="errorMessageWrap">{pwError}</div>}
       </div>
 
-      <div>
+      <div className="autologin fontBodyM">
         <input
           type="checkbox"
           checked={autoLogin}
@@ -81,18 +111,20 @@ export default function Login() {
       </div>
 
       <div>
-        <button onClick={handleLogin} className="bottomButton">
-          확인
+        <button onClick={handleLogin} className="bottomButton fontBodyM">
+          로그인
         </button>
       </div>
       <div>
         <KakaoLogin />
-        <NaverLogin />
       </div>
 
-      <div>
-        아직 회원이 아니신가요?{" "}
-        <button onClick={handleSignupRedirect} className="signupButton">
+      <div className="signup fontBodyM">
+        아직 웨더핏 회원이 아니시라면{" "}
+        <button
+          onClick={handleSignupRedirect}
+          className="signupButton fontBodyM"
+        >
           회원가입
         </button>
       </div>
