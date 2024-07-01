65f46589878494874b94c472cb9256c8a9b47a3c
GGU940 <nayung30@naver.com>
2024-07-01 11:36:09 +0900

    feat:로그인 정보 store 저장, nav에 반영

---
+++ src/store/loginInfoStore.js
@@ -0,0 +1,8 @@
+import create from 'zustand';
+
+const useLoginInfoStore = create((set) => ({
+  userInfo: null,
+  setUserInfo: (info) => set({ userInfo: info }),
+}));
+
+export { useLoginInfoStore };
