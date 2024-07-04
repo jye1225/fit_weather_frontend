9317d4d15693bb118693c5faebb74424a78c7064
yeeun <yeeun7117@sookmyung.ac.kr>
2024-07-04 09:20:45 +0900

    0703밤

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import "./css/common.css";
 
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode"; // jwt로 토큰 해석하는 jwt-decode 라이브러리 설치했습니다! :npm install jwt-decode
+import { url } from "./store/ref";
 
 import IndexPage from "./pages/IndexPage";
 import CommunityPage from "./pages/CommunityPage";
@@ -39,33 +45,36 @@ import CommuCollLike from "./pages/CommuCollLike";
 function App() {
   const navigate = useNavigate();
 
-  const { userInfo, setUserid, setUsername, setUserprofile } = useLoginInfoStore();
+  const { userInfo, setUserid, setUsername, setUserprofile } =
+    useLoginInfoStore();
   const [token, setToken] = useState(localStorage.getItem("token")); // token 상태를 추가합니다.
 
   useEffect(() => {
     const fetchData = async () => {
-      const loginToken = localStorage.getItem("token"); //
+      // const loginToken = localStorage.getItem("token");
 
-      if (loginToken) {
-        console.log('token 확인', loginToken); //일반로그인 : 토큰에 .이 있음 / 카카오로그인: 토큰에 . 없음 단순문자열임
+      if (token) {
+        console.log("token 확인", token); //일반로그인 : 토큰에 .이 있음 / 카카오로그인: 토큰에 . 없음 단순문자열임
 
-        if (loginToken.includes(".")) {// . 있음 => 일반로그인 ***
+        if (token.includes(".")) {
+          // . 있음 => 일반로그인 ***
           try {
-            const decodedToken = jwtDecode(loginToken);
+            const decodedToken = jwtDecode(token);
             setUserid(decodedToken.userid);
             setUsername(decodedToken.username);
             // setUserprofile(나중에처리..)
-            console.log(decodedToken);// 여기선 잘뜸.
+            console.log(decodedToken); // 여기선 잘뜸.
           } catch (error) {
             console.error("Invalid token", error);
             localStorage.removeItem("token");
           }
-
-        } else {// . 없음 => 카카오로그인 ***
+        } else {
+          // . 없음 => 카카오로그인 ***
           try {
-            await getUserData(loginToken);
-            document.cookie = `token=${loginToken}; path=/;`;//쿠키에도 토큰 저장!!
-            console.log('카카오 로그인으로 분류됨');//ok
+            const user = await getUserData(token);
+            await getUserData(token);
+            document.cookie = `token=${token}; path=/;`; //쿠키에도 토큰 저장!!
+            console.log("카카오 로그인으로 분류됨"); //ok
           } catch (err) {
             // 1_2.  localStorage에 저장된 token이 만료되었다면 token을 삭제하고 null로 업데이트
             console.log(err);
@@ -76,14 +85,13 @@ function App() {
       }
     };
     fetchData();
-  }, [token])
+  }, [token]);
 
-
-  const getUserData = async (loginToken) => {
+  const getUserData = async (token) => {
     // 2. Token을 이용하여 카카오 서버에서 인증을 거쳐 사용자 정보를 가져옴
     const response = await fetch(`https://kapi.kakao.com/v2/user/me`, {
       headers: {
-        Authorization: `Bearer ${loginToken}`,
+        Authorization: `Bearer ${token}`,
         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
       },
     });
@@ -92,12 +100,39 @@ function App() {
     setUserid(user.id);
     setUsername(user.properties.nickname);
     setUserprofile(user.properties.profile_image);
-    // console.log('카카오로그인 정보 확인 ', user);//ok
+
+    await registerKakaoUser(
+      //카카오로그인 함수
+      user.id,
+      user.properties.nickname,
+      user.properties.profile_image
+    );
+    console.log("카카오로그인 정보 확인 ", user);
 
     // navigate("/");//없어야 다른 카테고리도 이동됨.. => 갇혔던 이유 ㅠㅠ
   };
 
-
+  const registerKakaoUser = async (userid, username, profile_image) => {
+    try {
+      const response = await fetch(`${url}/kakao-register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userid,
+          username,
+          profile_image,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to register Kakao user");
+      }
+      setUserid(userid);
+      setUsername(username);
+      setUserprofile(profile_image);
+    } catch (error) {
+      console.error("Error registering Kakao user", error);
+    }
+  };
 
   // useEffect(() => {
   //   console.log('---userInfo---', userInfo); // 이렇게 해도 {msg: 'too long for access token.', code: -2} 오류 떠
