8731a75c70fd02372b138f02fc432404d344ebb9
yeeun <yeeun7117@sookmyung.ac.kr>
2024-07-04 16:03:34 +0900

    취향조사페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./css/common.css";
-import { url } from "./store/ref";
 
 import {
   BrowserRouter as Router,
@@ -9,7 +8,6 @@ import {
 } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode"; // jwt로 토큰 해석하는 jwt-decode 라이브러리 설치했습니다! :npm install jwt-decode
-import { url } from "./store/ref";
 
 import IndexPage from "./pages/IndexPage";
 import CommunityPage from "./pages/CommunityPage";
@@ -33,7 +31,8 @@ import Signup from "./pages/Signup";
 import KakaoLogin from "./pages/login/KakaoLogin";
 import Auth from "./pages/login/Auth";
 // import KakaoOauth from "./pages/login/KakaoOauth";
-import SignupComplete from "./pages/Signupcomplete";
+import SignupComplete from "./pages/SignupComplete";
+import MyStyle from "./pages/MyStyle"; // 취향조사 페이지
 import { useLoginInfoStore } from "./store/loginInfoStore";
 import Footer from "./components/Footer";
 
@@ -42,9 +41,8 @@ import CommuCollectionPage from "./pages/CommuCollectionPage";
 import CommuCollTalk from "./pages/CommuCollTalk";
 import CommuCollCmnt from "./pages/CommuCollCmnt";
 import CommuCollLike from "./pages/CommuCollLike";
-import MypageMain from './pages/MypageMain';
-import { url } from './store/ref';
-
+import MypageMain from "./pages/MypageMain";
+import { url } from "./store/ref";
 
 function App() {
   const navigate = useNavigate();
@@ -112,36 +110,8 @@ function App() {
       user.properties.nickname,
       user.properties.profile_image
     );
-     console.log('카카오로그인 정보 확인 ', user);//0703 ok
+    console.log("카카오로그인 정보 확인 ", user); //0703 ok
     // -------
-
-  };
-
-  const registerKakaoUser = async (userid, username, profile_image) => {
-    try {
-      const response = await fetch(`${url}/kakao-register`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          userid,
-          username,
-          profile_image,
-          // password: "",
-          // gender: "",
-        }),
-      });
-      if (!response.ok) {
-        throw new Error("Failed to register Kakao user");
-      }
-
-      console.log(userid, username, profile_image);
-
-      setUserid(userid);
-      setUsername(username);
-      setUserprofile(profile_image);
-    } catch (error) {
-      console.error("Error registering Kakao user", error);
-    }
   };
 
   const registerKakaoUser = async (userid, username, profile_image) => {
@@ -170,9 +140,9 @@ function App() {
   };
 
   useEffect(() => {
-    console.log('---userInfo---', userInfo);
+    console.log("---userInfo---", userInfo);
   }, [userInfo]);
-  
+
   return (
     <div className="App">
       <Routes>
@@ -205,8 +175,9 @@ function App() {
         <Route path="/oauth" element={<Auth />} />ㄴ
         <Route path="/oauth/kakao" element={<Auth />} />
         <Route path="/signupcomplete" element={<SignupComplete />} />
+        <Route path="/mystyle" element={<MyStyle />} />
         {/* 마이페이지 */}
-        <Route path='/mypage' element={<MypageMain />} />
+        <Route path="/mypage" element={<MypageMain />} />
         {/* 마이페이지 - 커뮤니티 활동 */}
         <Route path="/comuCollect" element={<CommuCollectionPage />}>
           <Route path="" element={<CommuCollTalk />} />
---
+++ src/pages/MyStyle.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import style from "../css/MyStyle.module.css";
+import HeaderMyStyle from "../components/HeaderMyStyle.jsx"; //취향조사 헤더
+
+function MyStyle() {
+  const [selectedButtons, setSelectedButtons] = useState({
+    tops: [],
+    bottoms: [],
+    outers: [],
+    others: [],
+  });
+
+  const handleButtonClick = (category, value) => {
+    //버튼 선택된 값 관리
+    setSelectedButtons((prev_value) => {
+      const category_value = prev_value[category]; // 이전 값 기반으로 현재 카테고리의 버튼 배열 가져옴
+      const isSelected = category_value.includes(value); // 선택된 버튼이 이미 선택된 상태라면 true, 아니라면 false 반환
+      const updated_value = isSelected
+        ? category_value.filter((item) => item !== value) //true일 경우 : 버튼 배열에서 value를 제거한 새로운 배열을 생성
+        : [...category_value, value]; //false일 경우 : [...] 사용하여 기존 배열에 value를 추가한 새로운 배열 생성
+      return { ...prev_value, [category]: updated_value }; // 새로운 상태 객체 반홤하며 업데이트
+    });
+  };
+
+  const renderButtons = (category, items) => {
+    // 버튼 렌더링
+    return items.map(
+      (
+        item //map 함수로 버튼 생성 : 중복 위해
+      ) => (
+        <button
+          key={item} //item이 티셔츠라면 key는 티셔츠가 됨
+          className={`fontBodyM ${
+            selectedButtons[category].includes(item) ? style.selected : ""
+          }`} //category에 item이 포함되어 있는지 확인, 포함되어있으면 style.selected 클래스에 추가, 아니면 빈문자열 추가
+          onClick={() => handleButtonClick(category, item)}
+        >
+          {item}
+        </button>
+      )
+    );
+  };
+
+  const handleSave = () => {
+    // 로컬 스토리지에 저장
+    const button_value_save = JSON.stringify(selectedButtons);
+    localStorage.setItem("selectedButtons", button_value_save);
+    console.log("저장된 데이터:", button_value_save);
+    alert("선택한 값이 저장되었습니다!");
+  };
+
+  return (
+    <>
+      <HeaderMyStyle />
+      <div className={`mw ${style.page}`}>
+        <div className={`fontHead3 ${style.mystyleTitle}`}>
+          (선택) 평소에 어떤 옷을 자주 입으시나요?
+          <p className={`fontTitleL ${style.mystyleBody}`}>
+            선택한 취향에 따라 옷이 추천됩니다!
+            <span className={style.mystyleBody_span}>
+              선택하지 않아도 가입은 완료됩니다.
+            </span>
+          </p>
+        </div>
+
+        <div className={style.mystyleWrap}>
+          <h2 className="fontTitleL">상의</h2>
+          <div className={`fontBodyM ${style.button}`}>
+            {renderButtons("tops", [
+              "티셔츠",
+              "맨투맨",
+              "니트",
+              "후드티",
+              "셔츠/블라우스",
+            ])}
+          </div>
+        </div>
+
+        <div className={style.mystyleWrap}>
+          <h2 className="fontTitleL">하의</h2>
+          <div className={`fontBodyM ${style.button}`}>
+            {renderButtons("bottoms", [
+              "청바지",
+              "슬랙스",
+              "트레이닝 바지",
+              "면바지",
+            ])}
+          </div>
+        </div>
+
+        <div className={style.mystyleWrap}>
+          <h2 className="fontTitleL">아우터</h2>
+          <div className={`fontBodyM ${style.button}`}>
+            {renderButtons("outers", [
+              "가디건",
+              "자켓",
+              "코트",
+              "집업",
+              "롱패딩",
+              "숏패딩",
+              "조끼",
+            ])}
+          </div>
+        </div>
+
+        <div className={style.mystyleWrap}>
+          <h2 className="fontTitleL">기타(선택)</h2>
+          <div className={`fontBodyM ${style.button}`}>
+            {renderButtons("others", ["롱스커트", "미니스커트", "원피스"])}
+          </div>
+        </div>
+
+        <button
+          className={`fontBodyM ${style.bottomButton}`}
+          onClick={handleSave}
+        >
+          저장하기
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default MyStyle;
