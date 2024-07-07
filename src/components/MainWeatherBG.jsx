db3f58685f47f5364437f81158559e67e7c1b6eb
GGU940 <nayung30@naver.com>
2024-07-08 04:20:24 +0900

    비 gif 추가

--- src/components/MainWeatherBG.jsx
+++ src/components/MainWeatherBG.jsx
@@ -3,7 +3,7 @@ import bgStyle from '../css/MainWeatherBG.module.css'
 
 const MainWeatherBG = () => {
     const [SKY, setSKY] = useState('');    //SKY -> 1맑음  3구름많음 4흐림 
-    const [PTY, setPTY] = useState('');    //PTY -> 0없음 1비 2비/눈 3눈 4소나기 
+    const [PTY, setPTY] = useState(0);    //PTY -> 0없음 1비 2비/눈 3눈 4소나기 
 
     const [bgImg, setBgImg] = useState('');//건물이미지
     const bgfile = ['a', 'b', 'c', 'd'];//건물이미지 img/weatherBG/bg/a.png
@@ -20,8 +20,9 @@ const MainWeatherBG = () => {
         const storedPty = localStorage.getItem('pmPTY');
         if (storedSky && storedPty) {
             setSKY(storedSky);
-            // setSKY(4);//임시고정
+            setSKY(4);///임시고정
             setPTY(storedPty);
+            // setPTY(1);
         }
 
         bgLoad();
@@ -33,15 +34,20 @@ const MainWeatherBG = () => {
 
     return (
         <div className={bgStyle.MainweatherBG}>
-            <img className={bgStyle.skyWeather} src={`img/weatherBG/sky/0${SKY}-3.png`} alt="SKY" />
-            <img className={bgStyle.skySun} src={`img/weatherBG/sky/0${SKY}-2.png`} alt="SKY" />
-            <img className={bgStyle.skyCloud} src={`img/weatherBG/sky/0${SKY}-1.png`} alt="SKY" />
-            {/* <img className={bgStyle.skyCloud} src={`img/weatherBG/sky/0${SKY}.png`} alt="SKY" /> */}
+            {SKY ? (<>
+                <img className={bgStyle.skyWeather} src={`img/weatherBG/sky/0${SKY}-3.png`} alt="SKY" />
+                <img className={bgStyle.skySun} src={`img/weatherBG/sky/0${SKY}-2.png`} alt="SKY" />
+                <img className={bgStyle.skyCloud} src={`img/weatherBG/sky/0${SKY}-1.png`} alt="SKY" />
+            </>) : ''
+            }
+            {PTY !== 0 ?
+                < img className={bgStyle.rain} src={`img/weatherBG/sky/pty${PTY}.gif`} alt="bgImg" />
+                : ''
+            }
 
-            {/* <img className={bgStyle.skyWeather} src={`img/weatherBG/sky/${SKY}.png`} alt="SKY" /> */}
             <img className={bgStyle.bg} src={`img/weatherBG/bg/${bgImg || `loading`}.png`} alt="bgImg" />
+
         </div>
     )
 }
-
 export default MainWeatherBG
