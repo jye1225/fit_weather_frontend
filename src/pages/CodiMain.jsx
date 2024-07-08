d12aff55381b730af3e6ad5824f93b0d87ddddd3
MEC43 <myung0403@gmail.com>
2024-07-08 11:29:05 +0900

    test

--- src/pages/CodiMain.jsx
+++ src/pages/CodiMain.jsx
@@ -1,35 +1,34 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import style from "../css/Codi.module.css";
-import bgStyle from "../css/MainWeatherBG.module.css";
-import Footer from "../components/Footer";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import style from '../css/Codi.module.css';
+import bgStyle from '../css/MainWeatherBG.module.css';
+import Footer from '../components/Footer';
 
-import CodiGuidelineBox from "../components/CodiGuidelineBox";
-import MainweatherBG from "../components/MainWeatherBG";
-import Header from "../components/Header";
-import H2Codi from "../components/H2Codi";
-import CodyWeather from "../components/CodyWeather";
-import CodiTalk from "../components/CodiTalk";
-import Avatar from "../components/Avatar";
-import CodiLogBoxsMain from "../components/CodiLogBoxsMain";
+import CodiGuidelineBox from '../components/CodiGuidelineBox';
+import MainweatherBG from '../components/MainWeatherBG';
+import Header from '../components/Header';
+import H2Codi from '../components/H2Codi';
+import CodyWeather from '../components/CodyWeather';
+import CodiTalk from '../components/CodiTalk';
+import Avatar from '../components/Avatar';
+import CodiLogBoxsMain from '../components/CodiLogBoxsMain';
 
 const CodiMain = () => {
   const formatDate = (date) => {
     const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const day = String(date.getDate()).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
     return `${year}${month}${day}`;
   };
   const today = new Date();
   const [selectDate, setSelectDate] = useState(formatDate(today));
 
   const [matchingUrl, setMatchingUrl] = useState({
-    tops: "",
-    bottoms: "",
-    outers: "",
+    tops: '',
+    bottoms: '',
+    outers: '',
   });
 
-  const [selectDate, setSelectDate] = useState("오늘");
   const [codiGuidelineBox, setCodiGuidelineBox] = useState(false);
 
   return (
@@ -51,11 +50,14 @@ const CodiMain = () => {
       <CodiLogBoxsMain />
 
       <div className={style.bigBtnCon}>
-        <Link to={"/codiLog"} className={`fontBodyM ${style.bigBtn}`}>
+        <Link to={'/codiLog'} className={`fontBodyM ${style.bigBtn}`}>
           <span>내 코디 기록</span>
           <img src="img/icons/common/calendar.svg" alt="" />
         </Link>
-        <button className={`fontBodyM ${style.bigBtn}`} onClick={() => setCodiGuidelineBox(true)}>
+        <button
+          className={`fontBodyM ${style.bigBtn}`}
+          onClick={() => setCodiGuidelineBox(true)}
+        >
           <span>기온별 추천 의류 전체보기</span>
           <img src="img/icons/common/alertSquare.svg" alt="" />
         </button>
@@ -63,11 +65,14 @@ const CodiMain = () => {
 
       <Footer />
 
-      {codiGuidelineBox === true ?
-        <CodiGuidelineBox codiGuidelineBox={codiGuidelineBox} setCodiGuidelineBox={setCodiGuidelineBox} />
-        : ''
-      }
-
+      {codiGuidelineBox === true ? (
+        <CodiGuidelineBox
+          codiGuidelineBox={codiGuidelineBox}
+          setCodiGuidelineBox={setCodiGuidelineBox}
+        />
+      ) : (
+        ''
+      )}
     </main>
   );
 };
