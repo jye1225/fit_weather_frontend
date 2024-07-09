0aee5e438e96ffe7cb15386938a44def4f12b777
eun <157719141+jye1225@users.noreply.github.com>
2024-07-09 11:01:22 +0900

    Update ref.js

--- src/store/ref.js
+++ src/store/ref.js
@@ -1,2 +1,3 @@
-export const url = "https://localhost:8080";
+export const url = "https://app.cloudtype.io/@yeeun7117/fit-weather-backend:main/fit-weather-backend#";
+// export const url = "https://localhost:8080";
 // export const url = "http://localhost:8080";
