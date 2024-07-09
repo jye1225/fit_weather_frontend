867cda1b294cddf9a50eebeaa747f07597488526
59Youn <wltjs9659@gmail.com>
2024-07-09 17:11:58 +0900

    성별정보 받아옴

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,8 +1,34 @@
 import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useLoginInfoStore } from "../store/loginInfoStore";
 import style from "../css/Avatar.module.css";
 
 const Avatar = ({ topUrl, bottomUrl, outerUrl }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const { setUsergender } = useLoginInfoStore();
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [gender, setGender] = useState(null); // gender 상태 추가
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (token) {
+        if (token.includes(".")) {
+          try {
+            const decodedToken = jwtDecode(token);
+            setUsergender(decodedToken.gender);
+            setGender(decodedToken.gender); // gender 설정
+            // console.log("아바타 영역", decodedToken);
+          } catch (error) {
+            console.error("Invalid token", error);
+            localStorage.removeItem("token");
+          }
+        } else {
+          setToken(null); // token 상태를 업데이트합니다.
+        }
+      }
+    };
+    fetchData();
+  }, [token]);
 
   useEffect(() => {
     console.log("Avatar URLs:", { topUrl, bottomUrl, outerUrl });
@@ -35,8 +61,8 @@ const Avatar = ({ topUrl, bottomUrl, outerUrl }) => {
         <>
           <img
             className={style.person}
-            src="img/clothes/woman.svg"
-            alt="여자"
+            src={`img/clothes/${gender === "male" ? "man" : "woman"}.svg`}
+            alt={gender === "male" ? "남자" : "여자"}
           />
           {topUrl && <img className={style.top} src={topUrl} alt="상의" />}
           {bottomUrl && (
