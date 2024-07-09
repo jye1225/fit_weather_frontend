ac2c713a8f731952859f8efc26989440c3267aa4
eun <157719141+jye1225@users.noreply.github.com>
2024-07-09 11:01:59 +0900

    Update ref.js
    찐

--- src/store/ref.js
+++ src/store/ref.js
@@ -1,3 +1,3 @@
-export const url = "https://app.cloudtype.io/@yeeun7117/fit-weather-backend:main/fit-weather-backend#";
+export const url = "https://port-0-fit-weather-backend-lydr6a6pb95d7aa4.sel5.cloudtype.app";
 // export const url = "https://localhost:8080";
 // export const url = "http://localhost:8080";
