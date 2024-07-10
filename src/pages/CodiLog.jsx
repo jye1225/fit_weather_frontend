a9f7935a93c1bc050ae41f45f129adbb3a891988
GGU <103281004+GGU940@users.noreply.github.com>
2024-07-10 11:16:41 +0900

    Update CodiLog.jsx

--- src/pages/CodiLog.jsx
+++ src/pages/CodiLog.jsx
@@ -15,9 +15,14 @@ import { useFeltOptionsStore } from '../store/codiStore'; // 태그 종류 가�
 import { useLoginInfoStore } from '../store/loginInfoStore';  //유저정보 import
 
 import { url } from "../store/ref";
+import { useCodiLogStore } from '../store/codiLogStore'; // Zustand 스토어 가져오기////////
 
 
 const CodiLog = () => {
+
+  const { setCodiLogLists, setAllCodiLogLists } = useCodiLogStore();/////////
+
+
   const navigate = useNavigate();
 
   const { feltOptions } = useFeltOptionsStore();
@@ -61,26 +66,60 @@ const CodiLog = () => {
     }
   };
 
-  const fetchLog = (page, reset = false) => {
-    if (!userInfo) { return };
+
+
+
+
+  const fetchLog = (page, limit, reset = false) => {
+    if (!userInfo) return;
 
     try {
       fetch(`${url}/codiLogList/${userInfo.userid}?page=${page}&limit=${limit}`)
-        .then((res) => res.json())//
+        .then((res) => res.json())
         .then((data) => {
-          if (reset) { // reset 파라미터가 true이면 데이터 초기화
-            setALLCodiLogList(data);
-            setCodiLogList(data);
+          if (reset) {
+            setAllCodiLogLists(data); // 전체 리스트 업데이트
+            setCodiLogLists(data); // 보여질 리스트 업데이트
           } else {
-            setALLCodiLogList(prev => [...prev, ...data]);
-            setCodiLogList(prev => [...prev, ...data]);
+            setAllCodiLogLists((prev) => [...prev, ...data]); // 전체 리스트 추가
+            setCodiLogLists((prev) => [...prev, ...data]); // 보여질 리스트 추가
           }
-        })
-
+        });
     } catch (error) {
       console.error('Error fetching codi log list:', error);
     }
-  }
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      fetchLog(0, 32, true); // 초기 데이터 가져오기
+    } else {
+      console.error('User info is not available');
+    }
+  }, [userInfo]);
+
+
+
+  // const fetchLog = (page, reset = false) => {
+  //   if (!userInfo) { return };
+
+  //   try {
+  //     fetch(`${url}/codiLogList/${userInfo.userid}?page=${page}&limit=${limit}`)
+  //       .then((res) => res.json())//
+  //       .then((data) => {
+  //         if (reset) { // reset 파라미터가 true이면 데이터 초기화
+  //           setALLCodiLogList(data);
+  //           setCodiLogList(data);
+  //         } else {
+  //           setALLCodiLogList(prev => [...prev, ...data]);
+  //           setCodiLogList(prev => [...prev, ...data]);
+  //         }
+  //       })
+
+  //   } catch (error) {
+  //     console.error('Error fetching codi log list:', error);
+  //   }
+  // }
 
   // const fetchLog = (page, reset = false) => {
   //   if (!userInfo) { return };
@@ -106,33 +145,18 @@ const CodiLog = () => {
   //     console.error('Error fetching codi log list:', error);
   //   }
   // }
- useEffect(() => {
-    if (userInfo) {  // userInfo가 유효한지 확인
-      setPage(0); // 페이지 번호 초기화
-      fetchLog(0, true); // 초기 데이터 가져오기, reset 파라미터를 true로 설정
-      console.log('[][][][][][][][][[] 실행')
-    } else {
-      console.error('User info is not available');
-    }
-
-    getToday();
-  }, []);
-  
-
-  useEffect(() => {
-    if (userInfo) {  // userInfo가 유효한지 확인
-      setPage(0); // 페이지 번호 초기화
-      fetchLog(0, true); // 초기 데이터 가져오기, reset 파라미터를 true로 설정
-    } else {
-      console.error('User info is not available');
-    }
-
-    getToday();
-  }, [codiView]);
 
 
+  // useEffect(() => {
+  //   if (userInfo) {  // userInfo가 유효한지 확인
+  //     setPage(0); // 페이지 번호 초기화
+  //     fetchLog(0, true); // 초기 데이터 가져오기, reset 파라미터를 true로 설정
+  //   } else {
+  //     console.error('User info is not available');
+  //   }
 
- 
+  //   getToday();
+  // }, [codiView]);
   // useEffect(() => {
   //   const storedALLCodiLogList = sessionStorage.getItem('ALLcodiLogList');// 세션 스토리지에서 전체 리스트 가져오기
   //   const storedCodiLogList = sessionStorage.getItem('codiLogList'); // 세션 스토리지에서 필터된 리스트 가져오기
@@ -151,7 +175,10 @@ const CodiLog = () => {
   // }, [codiView]);
 
 
-  
+
+
+
+
 
 
   useEffect(() => {
