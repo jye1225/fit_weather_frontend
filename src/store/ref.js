a7a08017d83da99aede7f7f19ab34591d564dc8d
eun <157719141+jye1225@users.noreply.github.com>
2024-07-10 10:14:41 +0900

    Update ref.js

--- src/store/ref.js
+++ src/store/ref.js
@@ -1,4 +1,3 @@
-// export const url =
-//   "https://port-0-fit-weather-backend-lydr6a6pb95d7aa4.sel5.cloudtype.app";
+export const url = "https://port-0-fit-weather-backend-lydr6a6pb95d7aa4.sel5.cloudtype.app";
 // export const url = "https://localhost:8080";
-export const url = "http://localhost:8080";
+// export const url = "http://localhost:8080";
