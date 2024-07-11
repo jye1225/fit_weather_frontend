dc904bcb0280bbde5c48852d6f9cb3b5a6c4dc7e
eun <157719141+jye1225@users.noreply.github.com>
2024-07-11 11:05:11 +0900

    Update Login.jsx
    login.jsx footer 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { url } from "../../store/ref";
 import KakaoLogin from "./KakaoLogin";
+import Footer from "../../components/Footer";
 
 const LoginPage = () => {
   const [userid, setUserid] = useState("");
@@ -47,6 +48,7 @@ const LoginPage = () => {
   };
 
   return (
+    <>
     <div className={`mw ${style.page}`}>
       <div className={style.logo} style={{ cursor: "pointer" }}>
         <img src="/img/logo/LogoL.svg" alt="Fit Weather Logo" />
@@ -105,6 +107,8 @@ const LoginPage = () => {
         </div>
       </div>
     </div>
+      </>
+    <Footer/>
   );
 };
 
